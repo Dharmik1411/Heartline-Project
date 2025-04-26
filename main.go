35a9e5fd6b9fd488fa5f1bd32898d0221758e340
Dharmik <73265503+Dharmik1411@users.noreply.github.com>
2025-04-26 01:22:34 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
+package main
+
+import (
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
+)
+
+func main() {
+    ConnectDatabase()
+
+    r := mux.NewRouter()
+
+    r.HandleFunc("/register", Register).Methods("POST")
+    r.HandleFunc("/login", Login).Methods("POST")
+    
+    secured := r.PathPrefix("/").Subrouter()
+    secured.Use(AuthMiddleware)
+    secured.HandleFunc("/profile", GetProfile).Methods("GET")
+    secured.HandleFunc("/profile", UpdateProfile).Methods("PATCH")
+
+    log.Println("Server started on :8080")
+    log.Fatal(http.ListenAndServe(":8080", r))
+}
 
